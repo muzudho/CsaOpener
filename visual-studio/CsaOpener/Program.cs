70bb8dd7254ba16e859e10919ca7e762210dc85d
むずでょ <muzudho1@gmail.com>
2019-04-04 22:06:27 +0900

    tochu

---
+++ visual-studio/CsaOpener/Program.cs
@@ -0,0 +1,158 @@
+﻿using System;
+
+[assembly: CLSCompliant(true)]
+namespace Grayscale.CsaOpener
+{
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Text;
+    using System.Threading;
+    using SevenZipExtractor;
+
+    /// <summary>
+    /// Entry point.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// Entry point.
+        /// </summary>
+        /// <param name="args">Command line arguments.</param>
+        public static void Main(string[] args)
+        {
+            var arguments = Arguments.Load(args);
+            Console.WriteLine($"Input directory: '{arguments.Input}', Output directory: '{arguments.Output}'.");
+
+            // カレントディレクトリ以下のファイルをすべて取得する
+            IEnumerable<string> files =
+                System.IO.Directory.EnumerateFiles(
+                    arguments.Input, "*", System.IO.SearchOption.AllDirectories);
+
+            int rest = 0;
+
+            // ファイルを列挙する
+            foreach (string f in files)
+            {
+                switch (Path.GetExtension(f).ToUpper())
+                {
+                    case ".CSA":
+                        {
+                            // そのままコピーすると名前がぶつかってしまう☆（＾～＾）
+                            var out_dir = Path.Combine(arguments.Output, $"copied-{Path.GetFileNameWithoutExtension(f)}");
+                            CreateDirectory(out_dir);
+                            var out_path = Path.Combine(out_dir, Path.GetFileName(f));
+                            Console.WriteLine($"コピー: {f} -> {out_path}");
+                            File.Copy(f, out_path, true);
+
+                            // エンコーディングを変える。
+                            ChangeEncodingToUtf8(out_path);
+                        }
+
+                        break;
+
+                    /*
+                case ".LZH":
+                    // 中に何入ってるか分からん。名前が被るかもしれない。
+                    var out_dir = Path.Combine(arguments.Output, $"extracted-{Path.GetFileNameWithoutExtension(f)}");
+                    Console.WriteLine($"LZHを解凍: {f} -> {out_dir}");
+                    Extract(f, out_dir);
+                    break;
+                    */
+
+                    case ".ZIP":
+                        {
+                            // 中に何入ってるか分からん。名前が被るかもしれない。
+                            var out_dir = Path.Combine(arguments.Output, $"extracted-{Path.GetFileNameWithoutExtension(f)}");
+                            Console.WriteLine($"ZIPを解凍: {f} -> {out_dir}");
+                            Extract(f, out_dir);
+
+                            // ゆっくり☆（＾～＾）！
+                            Thread.Sleep(60 * 1000);
+                        }
+
+                        break;
+
+                    default:
+                        Console.WriteLine($"むり: {f}");
+                        rest++;
+                        break;
+                }
+            }
+
+            Console.WriteLine($"まだ: {rest}");
+
+            int sleepSeconds = 15;
+            Console.WriteLine($"Finished. sleep: {sleepSeconds} sec.");
+            Thread.Sleep(sleepSeconds * 1000);
+        }
+
+        /// <summary>
+        /// ディレクトリーがなければ作るぜ☆（＾～＾）
+        /// </summary>
+        /// <param name="dir">パス。</param>
+        public static void CreateDirectory(string dir)
+        {
+            if (!Directory.Exists(dir))
+            {
+                Directory.CreateDirectory(dir);
+            }
+        }
+
+        /// <summary>
+        /// 解凍する。
+        /// </summary>
+        /// <param name="inputFile">入力ファイルパス。</param>
+        /// <param name="outputDirectory">出力ディレクトリーパス。</param>
+        public static void Extract(string inputFile, string outputDirectory)
+        {
+            var ar = new ArchiveFile(inputFile);
+
+            try
+            {
+                ar.Extract(outputDirectory, true);
+            }
+            catch (InvalidOperationException e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        /// <summary>
+        /// Shift-JIS と決めつけて、UTF8に変換する。
+        /// </summary>
+        /// <param name="file">ファイルパス。</param>
+        public static void ChangeEncodingToUtf8(string file)
+        {
+            byte[] bytesData;
+            // ファイルをbyte形で全て読み込み
+            using (FileStream fs1 = new FileStream(file, FileMode.Open))
+            {
+                byte[] data = new byte[fs1.Length];
+                fs1.Read(data, 0, data.Length);
+                fs1.Close();
+
+                // Shift-JIS -> UTF-8 変換（byte形）
+                Encoding sjisEnc = Encoding.GetEncoding("Shift_JIS");
+                string sjisstr = sjisEnc.GetString(data);
+                bytesData = System.Text.Encoding.UTF8.GetBytes(sjisstr);
+
+                // string型に変換したい場合はこんな感じに
+                // Encoding utf8Enc = Encoding.GetEncoding("UTF-8");
+                // string utf = utf8Enc.GetString(bytesData);
+                // Console.WriteLine(utf);
+            }
+
+            // 出力ファイルオープン（バイナリ形式）
+            using (FileStream fs2 = new FileStream(file, FileMode.Create))
+            {
+                // 書き込み設定（デフォルトはUTF-8）
+                BinaryWriter bw = new BinaryWriter(fs2);
+
+                // 出力ファイルへ全て書き込み
+                bw.Write(bytesData);
+                bw.Close();
+                fs2.Close();
+            }
+        }
+    }
+}
