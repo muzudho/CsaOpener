ba2093091d6aeee024a9bbd54accdf179118f70a
むずでょ <muzudho1@gmail.com>
2019-04-04 21:30:16 +0900

    init

---
+++ CsaOpener/Arguments.cs
@@ -0,0 +1,44 @@
+﻿namespace Grayscale.CsaOpener
+{
+    using CommandLine;
+    using CommandLine.Text;
+
+    /// <summary>
+    /// Command line arguments.
+    /// </summary>
+    public class Arguments
+    {
+        /// <summary>
+        /// Gets or sets a input directory.
+        /// </summary>
+        [Option('i', "input", Required = false, HelpText = "The input directory where the record is put.")]
+        public string Input { get; set; }
+
+        /// <summary>
+        /// Gets or sets a output directory.
+        /// </summary>
+        [Option('o', "output", Required = false, HelpText = "The output directory where the record is put.")]
+        public string Output { get; set; }
+
+        /// <summary>
+        /// 読取。
+        /// </summary>
+        /// <param name="args">コマンドライン引数。</param>
+        /// <returns>このオブジェクト。</returns>
+        public static Arguments Load(string[] args)
+        {
+            Arguments instance = null;
+
+            Parser.Default.ParseArguments<Arguments>(args).WithParsed(ins =>
+            {
+                // パース成功時
+                instance = (Arguments)ins;
+            }).WithNotParsed(err =>
+            {
+                // パース失敗時
+            });
+
+            return instance;
+        }
+    }
+}
---
+++ CsaOpener/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+[assembly: CLSCompliant(true)]
+namespace Grayscale.CsaOpener
+{
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Text;
+    using System.Threading;
+
+    /// <summary>
+    /// Entry point.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// Entry point.
+        /// </summary>
+        /// <param name="args">Command line arguments.</param>
+        public static void Main(string[] args)
+        {
+            var arguments = Arguments.Load(args);
+            Console.WriteLine($"Input directory: '{arguments.Input}', Output directory: '{arguments.Output}'.");
+
+            // カレントディレクトリ以下のファイルをすべて取得する
+            IEnumerable<string> files =
+                System.IO.Directory.EnumerateFiles(
+                    arguments.Input, "*", System.IO.SearchOption.AllDirectories);
+
+            // ファイルを列挙する
+            foreach (string f in files)
+            {
+                switch (Path.GetExtension(f).ToUpper())
+                {
+                    case ".CSA":
+                        Console.WriteLine($"そのままコピー: {f}");
+                        var out_path = Path.Combine(arguments.Output, Path.GetFileName(f));
+                        ChangeEncodingToUtf8(out_path);
+                        File.Copy(f, out_path, true);
+                        break;
+                    default:
+                        Console.WriteLine($"まだ: {f}");
+                        break;
+                }
+            }
+
+            int sleepSeconds = 15;
+            Console.WriteLine($"Finished. sleep: '{sleepSeconds}' sec.");
+            Thread.Sleep(sleepSeconds * 1000);
+        }
+
+        /// <summary>
+        /// Shift-JIS と決めつけて、UTF8に変換する。
+        /// </summary>
+        /// <param name="file">ファイルパス。</param>
+        public static void ChangeEncodingToUtf8(string file)
+        {
+            // ファイルをbyte形で全て読み込み
+            FileStream fs1 = new FileStream(file, FileMode.Open);
+            byte[] data = new byte[fs1.Length];
+            fs1.Read(data, 0, data.Length);
+            fs1.Close();
+
+            // Shift-JIS -> UTF-8 変換（byte形）
+            Encoding sjisEnc = Encoding.GetEncoding("Shift_JIS");
+            string sjisstr = sjisEnc.GetString(data);
+            byte[] bytesData = System.Text.Encoding.UTF8.GetBytes(sjisstr);
+
+            // string型に変換したい場合はこんな感じに
+            // Encoding utf8Enc = Encoding.GetEncoding("UTF-8");
+            // string utf = utf8Enc.GetString(bytesData);
+            // Console.WriteLine(utf);
+
+            // 出力ファイルオープン（バイナリ形式）
+            FileStream fs2 = new FileStream(file, FileMode.Create);
+
+            // 書き込み設定（デフォルトはUTF-8）
+            BinaryWriter bw = new BinaryWriter(fs2);
+
+            // 出力ファイルへ全て書き込み
+            bw.Write(bytesData);
+            bw.Close();
+            fs2.Close();
+        }
+    }
+}
