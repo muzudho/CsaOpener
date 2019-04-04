c24cb03871a398ab34cd9e2cbf5fab5f4a8945a1
むずでょ <muzudho1@gmail.com>
2019-04-04 23:05:17 +0900

    でけた

--- visual-studio/CsaOpener/Program.cs
+++ visual-studio/CsaOpener/Program.cs
@@ -10,6 +10,8 @@ namespace Grayscale.CsaOpener
     using System.Runtime.InteropServices;
     using System.Text;
     using System.Threading;
+    using ICSharpCode.SharpZipLib.GZip;
+    using ICSharpCode.SharpZipLib.Tar;
 
     /// <summary>
     /// Entry point.
@@ -43,11 +45,22 @@ namespace Grayscale.CsaOpener
                             var out_dir = Path.Combine(arguments.Output, $"copied-{Path.GetFileNameWithoutExtension(f)}");
                             CreateDirectory(out_dir);
                             var out_path = Path.Combine(out_dir, Path.GetFileName(f));
-                            Console.WriteLine($"コピー: {f} -> {out_path}");
-                            File.Copy(f, out_path, true);
 
-                            // エンコーディングを変える。
-                            ChangeEncodingToUtf8(out_path);
+                            // Console.WriteLine($"コピー: {f} -> {out_path}");
+                            File.Copy(f, out_path, true);
+                        }
+
+                        break;
+
+                    case ".KIF":
+                        {
+                            // そのままコピーすると名前がぶつかってしまう☆（＾～＾）
+                            var out_dir = Path.Combine(arguments.Output, $"copied-{Path.GetFileNameWithoutExtension(f)}");
+                            CreateDirectory(out_dir);
+                            var out_path = Path.Combine(out_dir, Path.GetFileName(f));
+
+                            // Console.WriteLine($"コピー: {f} -> {out_path}");
+                            File.Copy(f, out_path, true);
                         }
 
                         break;
@@ -56,8 +69,20 @@ namespace Grayscale.CsaOpener
                         {
                             // 中に何入ってるか分からん。名前が被るかもしれない。
                             var out_dir = Path.Combine(arguments.Output, $"extracted-{Path.GetFileNameWithoutExtension(f)}");
-                            Console.WriteLine($"LZHを解凍: {f} -> {out_dir}");
+
+                            // Console.WriteLine($"LZHを解凍: {f} -> {out_dir}");
                             Unlzh(f, out_dir);
+                        }
+
+                        break;
+
+                    case ".TGZ":
+                        {
+                            // 中に何入ってるか分からん。名前が被るかもしれない。
+                            var out_dir = Path.Combine(arguments.Output, $"extracted-{Path.GetFileNameWithoutExtension(f)}");
+
+                            // Console.WriteLine($"TGZを解凍: {f} -> {out_dir}");
+                            Untgz(f, out_dir);
                         }
 
                         break;
@@ -66,22 +91,38 @@ namespace Grayscale.CsaOpener
                         {
                             // 中に何入ってるか分からん。名前が被るかもしれない。
                             var out_dir = Path.Combine(arguments.Output, $"extracted-{Path.GetFileNameWithoutExtension(f)}");
-                            Console.WriteLine($"ZIPを解凍: {f} -> {out_dir}");
-                            Unzip(f, out_dir);
 
-                            // Extract(f, out_dir);
+                            // Console.WriteLine($"ZIPを解凍: {f} -> {out_dir}");
+                            Unzip(f, out_dir);
                         }
 
                         break;
 
                     default:
-                        Console.WriteLine($"むり: {f}");
-                        rest++;
+                        {
+                            // .exe とか解凍できないやつが入っている☆（＾～＾）！
+                            Console.WriteLine($"むり: {f}");
+
+                            // そのままコピーすると名前がぶつかってしまう☆（＾～＾）
+                            var out_dir = Path.Combine(arguments.Output, $"copied-{Path.GetFileNameWithoutExtension(f)}");
+                            CreateDirectory(out_dir);
+                            var out_path = Path.Combine(out_dir, Path.GetFileName(f));
+
+                            // Console.WriteLine($"コピー: {f} -> {out_path}");
+                            File.Copy(f, out_path, true);
+
+                            rest++;
+                        }
+
                         break;
                 }
             }
 
-            Console.WriteLine($"まだ: {rest}");
+            Console.WriteLine($"むり: {rest}");
+
+            // エンコーディングを変える。
+            Console.WriteLine("エンコーディング変換中...");
+            ChangeEncoding(arguments.Output);
 
             int sleepSeconds = 15;
             Console.WriteLine($"Finished. sleep: {sleepSeconds} sec.");
@@ -98,6 +139,24 @@ namespace Grayscale.CsaOpener
             {
                 Directory.CreateDirectory(dir);
             }
+        }
+
+        /// <summary>
+        /// 解凍する。
+        /// </summary>
+        /// <param name="inputFile">入力ファイルパス。</param>
+        /// <param name="outputDirectory">出力ディレクトリーパス。</param>
+        public static void Untgz(string inputFile, string outputDirectory)
+        {
+            Stream inStream = File.OpenRead(inputFile);
+            Stream gzipStream = new GZipInputStream(inStream);
+
+            TarArchive tarArchive = TarArchive.CreateInputTarArchive(gzipStream);
+            tarArchive.ExtractContents(outputDirectory);
+            tarArchive.Close();
+
+            gzipStream.Close();
+            inStream.Close();
         }
 
         /// <summary>
@@ -163,7 +222,6 @@ namespace Grayscale.CsaOpener
                 Console.WriteLine(e);
             }
         }
-        */
 
         /// <summary>
         /// ディレクトリー下のファイル数をカウントだぜ☆（＾～＾）
@@ -184,43 +242,67 @@ namespace Grayscale.CsaOpener
 
             return sum;
         }
+        */
 
         /// <summary>
         /// Shift-JIS と決めつけて、UTF8に変換する。
         /// </summary>
-        /// <param name="file">ファイルパス。</param>
-        public static void ChangeEncodingToUtf8(string file)
+        /// <param name="dir">ディレクトリ。</param>
+        public static void ChangeEncoding(string dir)
         {
-            byte[] bytesData;
-            // ファイルをbyte形で全て読み込み
-            using (FileStream fs1 = new FileStream(file, FileMode.Open))
+            foreach (var file in Directory.GetFiles(dir, "*", System.IO.SearchOption.AllDirectories))
             {
-                byte[] data = new byte[fs1.Length];
-                fs1.Read(data, 0, data.Length);
-                fs1.Close();
+                switch (Path.GetExtension(file).ToUpper())
+                {
+                    case ".CSA":
+                    case ".KIF":
+                        {
+                            byte[] bytesData;
 
-                // Shift-JIS -> UTF-8 変換（byte形）
-                Encoding sjisEnc = Encoding.GetEncoding("Shift_JIS");
-                string sjisstr = sjisEnc.GetString(data);
-                bytesData = System.Text.Encoding.UTF8.GetBytes(sjisstr);
+                            // ファイルをbyte形で全て読み込み
+                            using (FileStream fs1 = new FileStream(file, FileMode.Open))
+                            {
+                                byte[] data = new byte[fs1.Length];
+                                fs1.Read(data, 0, data.Length);
+                                fs1.Close();
 
-                // string型に変換したい場合はこんな感じに
-                // Encoding utf8Enc = Encoding.GetEncoding("UTF-8");
-                // string utf = utf8Enc.GetString(bytesData);
-                // Console.WriteLine(utf);
+                                // Shift-JIS -> UTF-8 変換（byte形）
+                                Encoding sjisEnc = Encoding.GetEncoding("Shift_JIS");
+                                string sjisstr = sjisEnc.GetString(data);
+                                bytesData = System.Text.Encoding.UTF8.GetBytes(sjisstr);
+
+                                // string型に変換したい場合はこんな感じに
+                                // Encoding utf8Enc = Encoding.GetEncoding("UTF-8");
+                                // string utf = utf8Enc.GetString(bytesData);
+                                // Console.WriteLine(utf);
+                            }
+
+                            // 出力ファイルオープン（バイナリ形式）
+                            using (FileStream fs2 = new FileStream(file, FileMode.Create))
+                            {
+                                // 書き込み設定（デフォルトはUTF-8）
+                                BinaryWriter bw = new BinaryWriter(fs2);
+
+                                // 出力ファイルへ全て書き込み
+                                bw.Write(bytesData);
+                                bw.Close();
+                                fs2.Close();
+                            }
+                        }
+
+                        break;
+                }
             }
 
-            // 出力ファイルオープン（バイナリ形式）
-            using (FileStream fs2 = new FileStream(file, FileMode.Create))
+            /*
+            // 再帰☆（＾～＾）！
+            // ショートカット踏んで 無限ループするなよ☆（＾～＾）
+            foreach (var subDir in Directory.GetDirectories(dir, "*", SearchOption.TopDirectoryOnly))
             {
-                // 書き込み設定（デフォルトはUTF-8）
-                BinaryWriter bw = new BinaryWriter(fs2);
-
-                // 出力ファイルへ全て書き込み
-                bw.Write(bytesData);
-                bw.Close();
-                fs2.Close();
+                // Console.WriteLine($"Sub dir: {subDir}.");
+                ChangeEncoding(subDir);
             }
+            */
         }
     }
 }
